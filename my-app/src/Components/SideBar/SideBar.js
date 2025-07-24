0db2dd7b12500d725c22b4d9d5edaec1c9419ac9
suhaas3 <suhaas3w@gmail.com>
2025-07-24 21:59:40 +0530

    open and close buttons for sidebar

--- my-app/src/Components/SideBar/SideBar.js
+++ my-app/src/Components/SideBar/SideBar.js
@@ -3,29 +3,19 @@ import { useNavigate } from 'react-router-dom';
 import './SideBar.css';
 
 function SideBar() {
-  const [mainSidebar, setMainSidebar] = useState(false);
+  const [openMainSidebar, setOpenMainSidebar] = useState(false);
+  const [closeMainSidebar,setCloseMainSidebar]= useState(true);
 
   const navigate = useNavigate();
 
-  const coursesObj = [
-    { course: 'Frontend' },
-    { course: 'backend' },
-    { course: 'Full stack' }
-  ]
+  function handleOpenSideBar() {
+    setOpenMainSidebar(prev => !prev)
+    setCloseMainSidebar(prev => !prev)
+  }
 
-  const webinarsObj = [
-    { webinar: 'resume preparation' },
-    { webinar: 'work shop' },
-    { webinar: 'block chain' }
-  ]
-  const clubsObj = [
-    { club: 'club-1' },
-    { club: 'club-2' },
-    { club: 'club-3' },
-  ]
-
-  function handleSideBar() {
-    setMainSidebar(prev => !prev)
+  function handleCloseSideBar() {
+    setCloseMainSidebar(prev => !prev);
+    setOpenMainSidebar(prev => !prev) 
   }
 
 
@@ -34,36 +24,38 @@ function SideBar() {
       <div className='sidebar-main-container'>
 
         <div className='sidebar-button-catiner'>
-          <button onClick={handleSideBar} className='sidebar-button'>&gt;&gt;</button>
+          {closeMainSidebar ? <button onClick={handleOpenSideBar} className='sidebar-open-button'>&gt;&gt;</button> : openMainSidebar}
+
+           {openMainSidebar ? <button onClick={handleCloseSideBar} className='sidebar-close-button'>&lt;&lt;</button> : closeMainSidebar}
         </div>
 
         <div className='sidebar-container'>
 
           <div className='sidebar-courses'>
               <img className='course-icon' src='https://cdn-icons-png.flaticon.com/512/4762/4762311.png' alt='...' />
-            {mainSidebar && <p data-name="Courses" className='sidebar-label' onClick={() => navigate('/courses')}>Courses</p>}
+            {openMainSidebar && <p data-name="Courses" className='sidebar-label' onClick={() => navigate('/courses')}>Courses</p>}
           </div>
 
           <div className='sidebar-webinars'>
             <img className='webinar-icon' src="https://cdn-icons-png.flaticon.com/512/2038/2038315.png" alt='...' />
-            {mainSidebar && <p data-name="Webinars" className='sidebar-label' onClick={() => navigate('/webinars')}>Webinars</p>}
+            {openMainSidebar && <p data-name="Webinars" className='sidebar-label' onClick={() => navigate('/webinars')}>Webinars</p>}
 
           </div>
 
           <div className='sidebar-clubs'>
             <img src='https://cdn-icons-png.flaticon.com/512/3990/3990804.png' alt='...' className='clubs-icon' />
-            {mainSidebar && <p data-name="Clubs" className='sidebar-label' onClick={() => navigate('/clubs')}>Clubs</p>}
+            {openMainSidebar && <p data-name="Clubs" className='sidebar-label' onClick={() => navigate('/clubs')}>Clubs</p>}
           </div>
 
           <div className='sidebar-cart'>
             <img src='https://cdn3.vectorstock.com/i/1000x1000/80/67/shopping-cart-icon-in-blue-silhouette-vector-18578067.jpg' alt='...' className='cart-icon' />
-            {mainSidebar && <p className='sidebar-label' onClick={() => navigate('/cart')}>Cart</p>}
+            {openMainSidebar && <p className='sidebar-label' onClick={() => navigate('/cart')}>Cart</p>}
 
           </div>
 
           <div className='sidebar-mydashboard'>
             <img src='https://cdn-icons-png.flaticon.com/512/8608/8608769.png' alt='...' className='profile-icon' />
-            {mainSidebar && <p className='sidebar-label' onClick={() => navigate('/userDetails')}>mydashboard</p>}
+            {openMainSidebar && <p className='sidebar-label' onClick={() => navigate('/userDetails')}>mydashboard</p>}
 
           </div>
 
